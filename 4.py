982c5f74afe0cc4e6f4a5f45520899db1823843f
ahruiz <ahruiz0@gmail.com>
2025-06-21 16:22:59 -0700

    adivina num generado aleatoriamente con 5 oport

---
+++ 4.py
@@ -0,0 +1,17 @@
+#codigo para adivinar numero oculto
+#generado aleatoriamente por el sistema
+import random
+
+print("*" * 90)
+print("Codigo para adivinar numero escondido con 5 oportunidades para acertar....")
+
+numEscon = random.randint(1, 100)
+#numEscon = 5
+
+for i in range(5):
+    numUsr = int(input(f"Ingresa el numero: "))
+    if numUsr == numEscon:
+        print(f"¡¡¡ACERTASTE!!!.. en tu intento numero { i + 1}.....Felicidades")
+        exit()
+    else:
+        print(f"¡¡¡FALLASTE!!!....te quedan {5 - (i + 1)} oportunidades")
